a9988371e11bd593c846b2a3f7d954627765fb6c
Maksym Heraskevych <heraskevych.maksym@gmail.com>
2019-04-20 10:02:32 +0300

    lessons3

--- js/main.js
+++ js/main.js
@@ -1,43 +1,17 @@
-// Tasks 0!
-
-// var map = function (fn, array) {
-// 	var newArray = [];
-// 	for (var i = 0; i < array.length; i++) {
-// 		newArray[i] = fn(array[i]);
-// 	}
-// 	return newArray;
-// };
-
-// Tasks 1!
-
-// var sequence = function (start = 0, step) {
-// 	var initVal = start;
-// 	return function() {	
-// 		var returnVal = initVal;
-// 		initVal += step;
-// 		return  returnVal;
-// 	}
-// };
-
-// var generator = sequence(10, 3);
-// var generator2 = sequence(7, 1);
-
-// console.log(generator()); // 10
-// console.log(generator()); // 13
-
-// console.log(generator2()); // 7
-
-// console.log(generator()); // 16
-
-// console.log(generator2()); // 8
+var radioSwitch = document.getElementsByClassName('radio1');
+var figure = document.getElementsByClassName('square');
 
 
-// Tasks 2!
+radioSwitch[0].onclick = function () {
+	figure[0].classList.add('area');
+};
 
-// var take = function (gen, x) {
-// 	var array1 = [];
-// 	for (var i = 0; i < x; i++) {
-// 		array1[i] = fn(array1[]);
-// 	}
-// 	return array1;
-// };
+radioSwitch[1].onclick = function () {
+	figure[0].classList.remove('area');
+};
+
+// 2 - Не получилось
+
+ myNumber.onchange = function() {
+    square.style.width = this.value +"px";
+  };
